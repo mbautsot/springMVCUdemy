ee0a3b281c5677b67486d91df685dc0b2aa64b10
mbautsot <marcosebautista@gmail.com>
2024-04-10 22:00:50 -0600

    MEBS SpringWEB

---
+++ springboot-web/src/main/java/com/marcos/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.marcos.curso.springboot.webapp.springbootweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class UserController {
+
+    @GetMapping("/details")
+    public String details(){
+        return "details";
+    }
+
+}
